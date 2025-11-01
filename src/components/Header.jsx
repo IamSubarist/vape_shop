539e3db87ac5649868c3aee7218084c7d23eaf90
IamSubarist <optimistroma927@gmail.com>
2025-11-01 16:54:52 +0300

    fix: header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,20 @@ import logo from "../assets/logo.svg";
 
 export const Header = () => {
   return (
-    <div className="flex items-center justify-center gap-2 h-full px-6">
+    <div className="flex items-center justify-start gap-2 h-full px-6">
       <img className="size-[80px]" src={logo} alt="logo" />
       <div className="flex flex-col justify-between">
-        <p
-          className="text-[36px] font-bold leading-[36px] text-transparent bg-clip-text"
-          style={{
-            backgroundImage:
-              "linear-gradient(to right, #7C3AED, #EC4899, #F97316, #06B6D4)",
-          }}
-        >
-          SmokeLabRND
+        <p className="text-[36px] font-bold leading-[36px] text-gray-100 relative logo-text">
+          <span
+            className="bg-gradient-to-r from-purple-400 via-pink-400 to-purple-400 bg-clip-text text-transparent"
+            style={{
+              backgroundSize: "200% auto",
+            }}
+          >
+            SmokeLabRND
+          </span>
         </p>
-        <p
-          className="text-[18px] leading-[18px] text-transparent bg-clip-text"
-          style={{
-            backgroundImage:
-              "linear-gradient(to right, #7C3AED, #EC4899, #F97316, #06B6D4)",
-          }}
-        >
+        <p className="text-[18px] leading-[18px] text-gray-400 font-medium">
           здесь еще какой-то текст...
         </p>
       </div>
